7243c7c9de2b264fe6e168a06bb9678425484ede
mariyaalv <pavlovskayama.maria@yandex.ru>
2024-06-21 00:19:43 +0300

    realised project

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,37 @@
-import arrow from 'src/images/arrow.svg';
+import clsx from 'clsx';
+import arrow from '../../images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
+import { RefObject } from 'react';
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+type ArrowButtonProps = {
+	isOpen: boolean;
+	onClick?: OnClick;
+	arrowButtonRef?: RefObject<HTMLDivElement>;
+};
+
+export const ArrowButton = ({
+	isOpen,
+	onClick,
+	arrowButtonRef,
+}: ArrowButtonProps) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={clsx(styles.container, isOpen && styles.container_open)}
+			onClick={onClick}
+			ref={arrowButtonRef}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={clsx(styles.arrow, isOpen && styles.arrow_open)}
+			/>
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,138 @@
-import { ArrowButton } from 'components/arrow-button';
-import { Button } from 'components/button';
+import { ArrowButton } from '../arrow-button';
+import { Button } from '../button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { Select } from '../select';
+import {
+	Dispatch,
+	FormEventHandler,
+	SetStateAction,
+	useEffect,
+	useRef,
+	useState,
+} from 'react';
+import {
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+	ArticleStateType,
+	defaultArticleState,
+} from '../../constants/articleProps';
+import { Separator } from '../separator';
+import clsx from 'clsx';
+import { Text } from '../text';
+import { RadioGroup } from '../radio-group';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	articleState: ArticleStateType;
+	setArticleState: (params: ArticleStateType) => void;
+	isOpen: boolean;
+	setIsOpen: Dispatch<SetStateAction<boolean>>;
+};
+
+export const ArticleParamsForm = ({
+	articleState,
+	setArticleState,
+	isOpen,
+	setIsOpen,
+}: ArticleParamsFormProps) => {
+	const [fontFamily, setFontFamily] = useState(articleState.fontFamilyOption);
+	const [fontSize, setFontSize] = useState(articleState.fontSizeOption);
+	const [fontColor, setFontColor] = useState(articleState.fontColor);
+	const [width, setWidth] = useState(articleState.contentWidth);
+	const [bgColor, setBgColor] = useState(articleState.backgroundColor);
+
+	const handleFormSubmit: FormEventHandler<HTMLFormElement> = (evt) => {
+		evt.preventDefault();
+	};
+
+	const handleButtonSubmit = (): void => {
+		setArticleState({
+			fontFamilyOption: fontFamily,
+			fontColor: fontColor,
+			backgroundColor: bgColor,
+			contentWidth: width,
+			fontSizeOption: fontSize,
+		});
+		setIsOpen(false);
+	};
+
+	const resetArticleState = () => {
+		setArticleState(defaultArticleState);
+		setFontFamily(defaultArticleState.fontFamilyOption);
+		setFontSize(defaultArticleState.fontSizeOption);
+		setFontColor(defaultArticleState.fontColor);
+		setWidth(defaultArticleState.contentWidth);
+		setBgColor(defaultArticleState.backgroundColor);
+	};
+
+	const arrowButtonRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		function handleArrowButtonClick(evt: MouseEvent) {
+			setIsOpen((isOpen) => !isOpen);
+			evt.stopPropagation();
+		}
+		arrowButtonRef.current?.addEventListener('click', handleArrowButtonClick);
+		return () => {
+			arrowButtonRef.current?.removeEventListener(
+				'click',
+				handleArrowButtonClick
+			);
+		};
+	}, []);
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton arrowButtonRef={arrowButtonRef} isOpen={isOpen} />
+			<aside
+				className={clsx(styles.container, isOpen && styles.container_open)}
+				onClick={(evt) => evt.stopPropagation()}>
+				<form className={styles.form} onSubmit={handleFormSubmit}>
+					<Text size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+					<Select
+						selected={fontFamily}
+						options={fontFamilyOptions}
+						onChange={(option) => setFontFamily(option)}
+						title='Шрифт'
+					/>
+					<RadioGroup
+						selected={fontSize}
+						options={fontSizeOptions}
+						onChange={(option) => setFontSize(option)}
+						name='fontSize'
+						title='Размер шрифта'
+					/>
+					<Select
+						selected={fontColor}
+						options={fontColors}
+						onChange={(option) => setFontColor(option)}
+						title='Цвет шифта'
+					/>
+					<Separator />
+					<Select
+						selected={bgColor}
+						options={backgroundColors}
+						onChange={(option) => setBgColor(option)}
+						title='Цвет фона'
+					/>
+					<Select
+						selected={width}
+						options={contentWidthArr}
+						onChange={(option) => setWidth(option)}
+						title='Ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
-						<Button title='Применить' type='submit' />
+						<Button title='Сбросить' type='reset' onClick={resetArticleState} />
+						<Button
+							title='Применить'
+							type='submit'
+							onClick={handleButtonSubmit}
+						/>
 					</div>
 				</form>
 			</aside>
